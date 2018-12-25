3e6b19d76025e00430060ea2d3a121634c499d48
romain.fontugne <romain.fontugne@gmail.com>
2018-12-25 11:27:10 +0900

    add file for index page

---
+++ src/GlobalReport.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li> <a href="/2497/asn/"> IIJ </a> </li>
+            <li> <a href="/3356/asn/"> Level(3) </a> </li>
+            <li> <a href="/2500/asn/"> WIDE </a> </li>
+            <li> <a href="/2501/asn/"> UT </a> </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+    },
+    data () {
+        return { }
+    },
+
+    methods: { },
+  }
+</script>
+
+<style>
+</style>
