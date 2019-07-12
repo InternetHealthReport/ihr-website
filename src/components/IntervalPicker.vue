1d5d388837e5f1d82a8734eefe6c07d913820218
Federico Mulas <mulas.federico.w@gmail.com>
2019-07-12 17:14:47 +0900

    Interval picker it's an independent component and AsAndIxp contain a working version of disconnection Events chart

---
+++ src/components/IntervalPicker.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <div class="row justify-around">
+      <date-time-picker
+        class="col-2"
+        :min="minTime"
+        :max="maxTime"
+        :value="localStartTime"
+        @input="localStartTime = $event; timeRange.min = $event.getTime();"
+      />
+      <q-range
+        class="col-7"
+        :min="minTime.getTime()"
+        :max="maxTime.getTime()"
+        v-model="timeRange"
+        label
+        drag-range
+        @change="updateRange"
+        :left-label-value="leftLabel"
+        :right-label-value="rigthLabel"
+      />
+      <date-time-picker
+        class="col-2"
+        :min="minTime"
+        :max="maxTime"
+        :value="localEndtime"
+        @input="localEndtime = $event; timeRange.max = $event.getTime();"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { debounce } from "quasar";
+import ReactiveChart from "@/components/ReactiveChart";
+import DateTimePicker from "@/components/DateTimePicker";
+import { PROJECT_START_DATE, Query } from "@/plugins/IhrApi";
+
+function timestampToUTC(timestamp) {
+  return new Date(timestamp).toUTCString();
+}
+
+const DEBOUNCE_TIME = 800;
+
+class ChartInterval {
+  constructor(begin, end) {
+    this.begin = begin;
+    this.end = end;
+  }
+
+  static today() {
+    let today = new Date();
+    return new ChartInterval(
+      new Date(today.setHours(0, 0, 0, 0)),
+      new Date(today.setHours(23, 59, 59, 0))
+    );
+  }
+
+  setIntevalTime(beginTimestamp, endTimestamp) {
+    this.begin.setTime(beginTimestamp);
+    this.end.setTime(endTimestamp);
+  }
+};
+
+export default {
+  components: {
+    ReactiveChart,
+    DateTimePicker
+  },
+  props: {
+    value: {
+      type: ChartInterval,
+      require: true
+    }
+  },
+  data() {
+    let debouncedEmit = debounce(
+      () => {
+        this.$emit("input", new ChartInterval(this.localStartTime, this.localEndtime));
+      },
+      DEBOUNCE_TIME,
+      false
+    );
+
+    return {
+      debouncedEmit: debouncedEmit,
+      minTime: PROJECT_START_DATE,
+      maxTime: new Date(),
+      localStartTime: new Date(this.value.begin),
+      localEndtime: new Date(this.value.end),
+      timeRange: { min: this.value.begin.getTime(), max: this.value.end.getTime() }
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    updateRange(range) {
+      //no reactivity using setTime, I'm sad to do a useless new....
+      this.localStartTime = new Date(this.timeRange.min);
+      this.localEndtime = new Date(this.timeRange.max);
+    }
+  },
+  computed: {
+    leftLabel() {
+      this.debouncedEmit();
+      return timestampToUTC(this.timeRange.min);
+    },
+    rigthLabel() {
+      this.debouncedEmit();
+      return timestampToUTC(this.timeRange.max);
+    }
+  }
+};
+
+export { ChartInterval };
+</script>
+
+<style lang="stylus"></style>
---
+++ src/views/charts/AsInterdependencies.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="IHR_disco-chart">
+    <reactive-chart
+      :layout="layout"
+      :traces="traces"
+      @loaded="loading = false"
+      chart-title="Disconnection Events"
+    />
+    <div v-if="loading" class="IHR_loading-spinner">
+      <q-spinner color="secondary" size="4em"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { debounce } from "quasar";
+import ReactiveChart from "@/components/ReactiveChart";
+import DateTimePicker from "@/components/DateTimePicker";
+import { DiscoEventQuery, PROJECT_START_DATE } from "@/plugins/IhrApi";
+
+export default {
+  components: {
+    ReactiveChart,
+    DateTimePicker
+  },
+  props: {
+    streamName: {
+      type: Number,
+      required: true
+    },
+    startTime: {
+      type: Date,
+      require: true
+    },
+    endTime: {
+      type: Date,
+      require: true
+    }
+  },
+  data() {
+    let filter = new DiscoEventQuery()
+      .streamName(this.streamName)
+      .timeInterval(this.startTime, this.endTime)
+      .orderedByStartTime();
+
+    //prevent calls within 500ms and execute only the last one
+    let debouncedApiCall = debounce(
+      () => {
+        this.queryDiscoApi();
+      },
+      800,
+      false
+    );
+
+    return {
+      debouncedApiCall: debouncedApiCall,
+      loading: true,
+      filter: filter,
+      traces: [
+        {
+          x: [],
+          y: [],
+          z: [],
+          yaxis: "y",
+          name: "Disconnection Level",
+          showlegend: false,
+          line: { shape: "hv" }
+        }
+      ],
+      layout: {
+        hovermode: "closest",
+        yaxis: {
+          title: "Disconnection Level",
+          autorange: true,
+          automargin: true
+        },
+        margin: {
+          t: 50,
+          b: 50
+        },
+        height: 350,
+        showlegend: true,
+        legend: {
+          x: 0,
+          y: 1.2,
+          orientation: "h"
+        }
+      }
+    };
+  },
+  mounted() {
+    this.debouncedApiCall();
+  },
+  methods: {
+    queryDiscoApi() {
+      this.loading = true;
+      this.$ihr_api.disco_events(
+        this.filter,
+        result => {
+          this.fetchDiscoData(result.results);
+          this.loading = false;
+        },
+        error => {
+          console.error(error); //FIXME do a correct alert
+        }
+      );
+    },
+    fetchDiscoData(data) {
+      let trace = this.traces[0];
+      trace.x = [];
+      trace.y = [];
+      trace.z = [];
+
+      trace.x.push(this.startTime.toUTCString());
+      trace.y.push(0);
+      trace.z.push(0);
+
+      data.forEach(elem => {
+        trace.x.push(elem.starttime);
+        trace.y.push(elem.avglevel);
+        trace.z.push(elem.id);
+
+        trace.x.push(elem.endtime);
+        trace.y.push(0);
+        trace.z.push(elem.id);
+      });
+
+      trace.x.push(this.endTime.toUTCString());
+      trace.y.push(0);
+      trace.z.push(0);
+    }
+  },
+  watch: {
+    startTime() {
+      this.filter.startTime(this.startTime, DiscoEventQuery.GTE);
+      this.debouncedApiCall();
+    },
+    endTime() {
+      this.filter.endTime(this.endTime, DiscoEventQuery.LTE);
+      this.debouncedApiCall();
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+.IHR_
+  &disco-chart
+    text-align center
+    position relative
+
+    & h1
+      font-size 25pt
+      margin-bottom 0px
+      font-weight 400
+      line-height 1
+    
+  &loading-spinner
+    position absolute
+    top 50%
+    left 49%
+</style>
