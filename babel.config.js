ae732c722ca4b22cd67882358e1bb2716b5c3bf8
Federico Mulas <mulas.federico.w@gmail.com>
2019-10-18 15:14:15 +0900

    added ignore console.log con build

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+const removeConsolePlugin = [];
+if (process.env.NODE_ENV === "production") {
+  removeConsolePlugin.push("transform-remove-console");
+}
+
 module.exports = {
   presets: ["@vue/app"],
   plugins: [
@@ -9,6 +14,7 @@ module.exports = {
           preventFullImport: true
         }
       }
-    ]
+    ],
+    removeConsolePlugin
   ]
 };
