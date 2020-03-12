6f009b09e2f75a4e597a64a2485bca68f5d29ec7
romain.fontugne <romain.fontugne@gmail.com>
2020-03-12 16:30:13 +0900

    Add API swagger ui

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
+      <div id="swagger-ui"></div>
   </div>
 </template>
 <script>
+import SwaggerUI, { presets } from 'swagger-ui'
 
 export default {
   components: {
@@ -10,9 +12,20 @@ export default {
   data() {
     return {
     }
+  },
+  mounted: function() {
+    const ui = SwaggerUI({
+        url: "https://ihr.iijlab.net/ihr/swagger.yaml",
+        dom_id: "#swagger-ui",
+        layout: "BaseLayout",
+        presets: [presets.apis],
+        deepLinking: false
+    });
   }
 };
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.version
+    color red
 </style>
