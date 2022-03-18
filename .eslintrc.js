93977a75d2e886b7aaae94ba1490e8da455a7dd5
akib22 <adnanakib75@gmail.com>
2022-03-18 21:11:33 +0600

    Fix 'lint' command running error

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    indent: ['error', 2, { SwitchCase: 1 }],
+    indent: ['error', 2, { SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] }],
     quotes: ['error', 'single', { avoidEscape: true }],
   },
   parserOptions: {
