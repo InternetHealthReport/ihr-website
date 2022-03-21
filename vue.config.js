9d29149ece7a02ad517b73548a0bd9f6b20bbe1f
Aniket762 <aniketindian8@gmail.com>
2022-03-21 13:30:39 +0530

    fix: minor errors in documentation

--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,50 @@
-const path = require("path");
+const path = require('path')
 module.exports = {
-  runtimeCompiler: true,
-  chainWebpack: webpackConfig => {
-    webpackConfig.module
-      .rule("ify-loader")
-      .test(/node_module\/plotly.js\/*.js$/)
-      .use("ify-loader")
-      .loader("ify-loader");
-
-    webpackConfig.module
-      .rule("i18n")
-      .resourceQuery(/blockType=i18n/)
-      .type("javascript/auto")
-      .use("i18n")
-      .loader("@kazupon/vue-i18n-loader")
-      .end();
-  },
-  pluginOptions: {
-    quasar: {
-      rtlSupport: true,
-      treeShake: true
+    runtimeCompiler: true,
+    devServer: {
+        compress: true,
+        overlay: {
+            warnings: false,
+            errors: true,
+        },
+        open: 'Google Chrome',
     },
-    i18n: {
-      locale: "en-us",
-      fallbackLocale: "en-us",
-      localeDir: "locales",
-      enableInSFC: true
-    }
-  },
-  transpileDependencies: [/[\\/]node_modules[\\/]quasar[\\/]/],
-  configureWebpack: {
-    resolve: {
-      alias: {
-        "@": path.join(__dirname, "./src"),
-        src: path.join(__dirname, "./src")
-      },
-      extensions: [".js", ".vue", ".json", ".styl"]
-    }
-  },
-  publicPath: "/ihr/"
-};
+    chainWebpack: webpackConfig => {
+        webpackConfig.module
+            .rule('ify-loader')
+            .test(/node_module\/plotly.js\/*.js$/)
+            .use('ify-loader')
+            .loader('ify-loader')
+
+        webpackConfig.module
+            .rule('i18n')
+            .resourceQuery(/blockType=i18n/)
+            .type('javascript/auto')
+            .use('i18n')
+            .loader('@kazupon/vue-i18n-loader')
+            .end()
+    },
+    pluginOptions: {
+        quasar: {
+            rtlSupport: true,
+            treeShake: true,
+        },
+        i18n: {
+            locale: 'en-us',
+            fallbackLocale: 'en-us',
+            localeDir: 'locales',
+            enableInSFC: true,
+        },
+    },
+    transpileDependencies: [/[\\/]node_modules[\\/]quasar[\\/]/],
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.join(__dirname, './src'),
+                src: path.join(__dirname, './src'),
+            },
+            extensions: ['.js', '.vue', '.json', '.styl'],
+        },
+    },
+    publicPath: '/ihr/',
+}
