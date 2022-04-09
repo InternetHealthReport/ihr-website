d2f9a9d838f66d2c016ff107f63246f675de7c09
akib22 <adnanakib75@gmail.com>
2022-04-09 14:42:33 +0600

    Feature - run 'lint' command before committing changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
+    'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     indent: ['error', 2, { SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] }],
