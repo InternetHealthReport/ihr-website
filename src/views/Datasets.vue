6f1e13063c948e63bf1ae5570635bfa878951373
Aniket762 <aniketindian8@gmail.com>
2022-06-14 18:11:02 +0530

    feat:http request for mlab init

--- src/views/Datasets.vue
+++ src/views/Datasets.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Datasets</h1>
+  <div>
+    <h1>Datasets</h1>
+    <button @click="getNetworks">Get Networks</button>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      networks: [],
+    };
+  },
+  methods: {
+    getNetworks() {
+      axios
+        .get(
+          "https://statistics.measurementlab.net/v0/NA/US/US-MD/2020/histogram_daily_stats.json"
+        )
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
