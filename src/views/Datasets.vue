3830c4b65b5cfdbedf9d4a360a7e4a1833220bd1
Aniket762 <aniketindian8@gmail.com>
2022-06-15 15:29:44 +0530

    feat:error message shown when api end point faulty

--- src/views/Datasets.vue
+++ src/views/Datasets.vue
@@ -2,6 +2,10 @@
   <div>
     <h1>Datasets</h1>
     <button @click="getNetworks">Get Networks</button>
+    <h3 v-if="errorMsg">{{ errorMsg }}</h3>
+    <div v-for="network in networks" :key="network.id">
+      <h3>{{ network.date }} - {{ network.country_code }}</h3>
+    </div>
   </div>
 </template>
 
@@ -11,19 +15,22 @@ export default {
   data() {
     return {
       networks: [],
+      errorMsg: "",
     };
   },
   methods: {
     getNetworks() {
       axios
         .get(
-          "https://statistics.measurementlab.net/v0/NA/US/US-MD/2020/histogram_daily_stats.json"
+          "https://statttistics.measurementlab.net/v0/NA/US/US-MD/2020/histogram_daily_stats.json"
         )
         .then((response) => {
           console.log(response.data);
+          this.networks = response.data;
         })
         .catch((error) => {
           console.log(error);
+          this.errorMsg = "MLab API end point not working";
         });
     },
   },
