6b75fb9bc5d1f4cb408ab5bfcf562b2624f5cbb8
Lori <luofeica@seas.upenn.edu>
2022-06-22 10:53:58 -0700

    Wip the user login and register process and redo the UI

--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -8,28 +8,57 @@
         </template>
       </q-banner>
     </transition>
-    <h1>{{ $t("sigIn.login") }}</h1>
-    <div class="shadow-2" id="IHR_sig-in-form-container">
-      <q-input v-model="email" label="email" type="email"
-        :rules="[val => $ihrStyle.validateEmail(val) || $t('forms.fancyEmail')]">
+    <h1>{{ $t("sigIn.register") }}</h1>
+    <div class="shadow-2" id="IHR_sig-in-form-container" >
+      <q-input
+        v-model="email"
+        label="email"
+        type="email"
+        :rules="[val => $ihrStyle.validateEmail(val) || $t('forms.fancyEmail')]"
+      >
         <template v-slot:prepend>
           <q-icon name="fa fa-envelope" />
         </template>
       </q-input>
-      <q-input v-model="password" label="password" :type="isPwd ? 'password' : 'text'" :rules="[
-        val => $ihrStyle.validatePassword(val) || $t('forms.weakPassword')
-      ]">
+      <q-input
+        v-model="password"
+        label="password"
+        :type="isPwd ? 'password' : 'text'"
+        :rules="[
+          val => $ihrStyle.validatePassword(val) || $t('forms.weakPassword')
+        ]"
+      >
         <template v-slot:prepend>
           <q-icon name="fa fa-key" />
         </template>
         <template v-slot:append>
-          <q-icon :name="isPwd ? 'far fa-eye' : 'far fa-eye-slash'" class="cursor-pointer" @click="isPwd = !isPwd" />
+          <q-icon
+            :name="isPwd ? 'far fa-eye' : 'far fa-eye-slash'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
         </template>
       </q-input>
-      <div :style="{
-        height: recaptcha_loaded ? 'auto' : '90px',
-        position: 'relative'
-      }">
+      <q-input
+        v-model="code"
+        label="code"
+        :rules="[
+          val => $ihrStyle.validateCode(val) || $t('forms.weakCode')
+        ]"
+      >
+        <template v-slot:prepend>
+          <q-icon name="fa fa-check" />
+        </template>
+        <template v-slot:append>
+          <q-btn @click="sendCode" color="secondary" no-caps>send</q-btn>
+        </template>
+      </q-input>
+      <div
+        :style="{
+          height: recaptcha_loaded ? 'auto' : '90px',
+          position: 'relative'
+        }"
+      >
         <!-- <vue-recaptcha
           :sitekey="$ihrStyle.recaptchaKey"
           id="IHR_sig-in-captcha"
@@ -43,24 +72,20 @@
       </div>
       <!-- <div>{{ $t("sigIn.mailWillBeSent") }}</div> -->
       <div style="display:flex;justify-content:space-between;">
-        <router-link to="register">Register</router-link>
-        <router-link to="reset_password">Forget Password?</router-link>
+        <router-link to="login">Login</router-link>
       </div>
-      <q-btn color="positive" @click="login">login</q-btn>
+      <q-btn color="positive" @click="validateAndSend">register</q-btn>
     </div>
-    <!-- <div class="shadow-2" id="IHR_confirm-your-email" v-else>
-      <div>{{ $t("sigIn.emailSentTo") }}</div>
-      <div id="IHR_email-confirmation">{{ email }}</div>
-      <div>{{ $t("sigIn.pleaseFollowTheLink") }}</div>
-    </div> -->
-    <q-dialog v-model="emailSent">
+    <q-dialog
+      v-model="emailSent"
+    >
       <q-card style="width: 300px">
         <q-card-section>
           <div class="text-h6">Alert</div>
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          {{ message }}
+          {{message}}
         </q-card-section>
 
         <q-card-actions align="right" class="bg-white text-teal">
@@ -76,12 +101,14 @@
 
 export default {
   // components: { VueRecaptcha },
+  name:'RegisterPage',
   data() {
     return {
       email: "",
       password: "",
       recaptcha: "",
-      message: "",
+      code: "",
+      message:"",
       emailSent: false,
       isPwd: true,
       recaptcha_loaded: false,
@@ -89,10 +116,10 @@ export default {
     };
   },
   mounted() {
-    // this.$libraryDelayer.load("google_recaptcha", () => {
-    //   this.recaptcha_loaded = true;
-    //   this.$libraryDelayer.getRidOfInlineStyle("IHR_sig-in-captcha", "div");
-    // });
+    this.$libraryDelayer.load("google_recaptcha", () => {
+      this.recaptcha_loaded = true;
+      this.$libraryDelayer.getRidOfInlineStyle("IHR_sig-in-captcha", "div");
+    });
   },
   methods: {
     expired() {
@@ -105,7 +132,7 @@ export default {
     ensureCss(id) {
       this.$libraryDelayer.getRidOfInlineStyle(id, "div");
     },
-    login() {
+    validateAndSend() {
       this.errors = [];
       // this.recaptcha != "" || this.errors.push("missingReCaptcha");
       this.$ihrStyle.validatePassword(this.password) ||
@@ -113,23 +140,32 @@ export default {
       this.$ihrStyle.validateEmail(this.email) ||
         this.errors.push("strangeEmail");
       if (this.errors.length == 0)
-        this.$ihr_api.userLogin(
+        this.$ihr_api.userSignIn(
           this.email,
           this.password,
-          res => {
+          this.code,
+          (res) => {
             this.emailSent = true
             this.message = res.msg
-            if (res.code === 200) {
-              this.$emit('isLogin', true)
-              this.$router.push('/en-us')
-            }
           },
           error => {
             this.emailSent = true
-            this.message = error.data.detail
+            this.message = error.detail
           }
         );
+    },
+    sendCode() {
+      this.$ihr_api.sendsendregisteremail(this.email,
+        (res) => {
+          this.emailSent = true
+          this.message = res.msg
+        },
+        (error) => {
+            this.emailSent = true
+            this.message = error.detail
+          })
     }
+
   },
   computed: {
   }
--- src/views/user/ResetPassword.vue
+++ src/views/user/ResetPassword.vue
@@ -8,63 +8,91 @@
         </template>
       </q-banner>
     </transition>
-    <h1>{{ title }}</h1>
-    <div class="row justify-around IHR_content" v-if="$ihr_api.authenticated">
-      <q-btn color="secondary" class="col-3" @click="$router.push({ name: 'personal_page' })">{{ $t('personalPage.title') }}</q-btn>
-    </div>
-    <div class="shadow-2" id="IHR_reset-password-form" v-else-if="!emailSent">
-      <q-input v-model="email" label="email" type="email" :rules="[val => $ihrStyle.validateEmail(val) || $t('forms.fancyEmail')]">
+    <h1>{{ $t('resetPassword.title') }}</h1>
+    <!-- <div class="row justify-around IHR_content" v-if="$ihr_api.authenticated">
+            <q-btn color="secondary" class="col-3" @click="$router.push({ name: 'personal_page' })">{{ $t('personalPage.title') }}</q-btn>
+        </div> -->
+    <div class="shadow-2" id="IHR_reset-password-form">
+      <q-input v-model="email" label="email" type="email"
+        :rules="[val => $ihrStyle.validateEmail(val) || $t('forms.fancyEmail')]">
         <template v-slot:prepend>
-          <q-icon name="far fa-envelope" />
+          <q-icon name="fa fa-envelope" />
         </template>
       </q-input>
-      <div>
-        <password-confirm v-model="password" v-if="token != undefined" ref="password" />
-      </div>
-      <div
-        :style="{
-          height: recaptcha_loaded ? 'auto' : '90px',
-          position: 'relative',
-        }"
-      >
-        <vue-recaptcha
+      <q-input v-model="password" label="new_password" :type="isPwd ? 'password' : 'text'"
+        :rules="[val => $ihrStyle.validatePassword(val) || $t('forms.weakPassword')]">
+        <template v-slot:prepend>
+          <q-icon name="fa fa-key" />
+        </template>
+        <template v-slot:append>
+          <q-icon :name="isPwd ? 'far fa-eye' : 'far fa-eye-slash'" class="cursor-pointer" @click="isPwd = !isPwd" />
+        </template>
+      </q-input>
+      <q-input v-model="code" label="code" :rules="[val => $ihrStyle.validateCode(val) || $t('forms.weakCode')]">
+        <template v-slot:prepend>
+          <q-icon name="fa fa-check" />
+        </template>
+        <template v-slot:append>
+          <q-btn @click="sendCode" color="secondary" no-caps>send</q-btn>
+        </template>
+      </q-input>
+      <div :style="{
+        height: recaptcha_loaded ? 'auto' : '90px',
+        position: 'relative',
+      }">
+        <!-- <vue-recaptcha
           :sitekey="$ihrStyle.recaptchaKey"
           id="IHR_sig-in-captcha"
           @verify="verify"
           @expired="expired"
           :render="ensureCss"
-        ></vue-recaptcha>
+        ></vue-recaptcha> -->
         <q-inner-loading :showing="!recaptcha_loaded">
           <q-spinner-gears size="50px" color="primary" />
         </q-inner-loading>
       </div>
-      <q-btn color="secondary" @click="token != undefined ? validateAndChange() : validateAndSend()">
-        {{ $t(`resetPassword.${token != undefined ? 'resetPassword' : 'recoverPassword'}`) }}
+      <q-btn color="positive" @click="forgetpassword()">
+        {{ $t('resetPassword.resetPassword') }}
       </q-btn>
     </div>
-    <div class="shadow-2" id="IHR_confirm-your-email" v-else>
-      <div>{{ $t('resetPassword.instructionPart1') }}</div>
-      <div id="IHR_email-confirmation">{{ email }}</div>
-      <div>{{ $t('resetPassword.instructionPart2') }}</div>
-    </div>
+    <!-- <div class="shadow-2" id="IHR_confirm-your-email" v-else>
+            <div>{{ $t('resetPassword.instructionPart1') }}</div>
+            <div id="IHR_email-confirmation">{{ email }}</div>
+            <div>{{ $t('resetPassword.instructionPart2') }}</div>
+        </div> -->
+    <q-dialog v-model="emailSent">
+      <q-card style="width: 300px">
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ message }}
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white text-teal">
+          <q-btn flat label="OK" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
 <script>
-import VueRecaptcha from 'vue-recaptcha'
-import PasswordConfirm from '@/components/forms/PasswordConfirm'
+// import VueRecaptcha from "vue-recaptcha";
 
 export default {
   components: {
-    VueRecaptcha,
-    PasswordConfirm,
+    // VueRecaptcha,
   },
   data() {
     return {
       email: '',
       recaptcha: '',
       password: '',
-      token: this.$route.query.token,
+      code: '',
+      isPwd: true,
+      message: '',
       error: null,
       emailSent: false,
       recaptcha_loaded: false,
@@ -86,66 +114,49 @@ export default {
     ensureCss(id) {
       this.$libraryDelayer.getRidOfInlineStyle(id, 'div')
     },
-    validateAndSend() {
-      if (!this.$ihrStyle.validateEmail(this.email)) {
-        this.error = 'InvalidEmail'
-        return
-      }
-      if (this.recaptcha == '') {
-        this.error = 'AreYouRobot'
-        return
-      }
-      this.$ihr_api.userResetPasswordRequest(
+    sendCode() {
+      this.$ihr_api.sendforgetpasswordemail(
         this.email,
-        this.recaptcha,
-        () => {
+        res => {
           this.emailSent = true
-          this.error = null
+          this.message = res.msg
         },
         error => {
-          this.error = error.status
-          console.log(error.detail)
+          this.emailSent = true
+          this.message = error.detail
         }
       )
     },
-    validateAndChange() {
+    forgetpassword() {
       if (!this.$ihrStyle.validateEmail(this.email)) {
         this.error = 'InvalidEmail'
         return
       }
-      if (!this.$refs['password'].isValid()) {
-        this.error = this.password.toString()
+      if (!this.$ihrStyle.validatePassword(this.password)) {
+        this.error = 'passwordTooWeak'
         return
       }
-      if (this.recaptcha == '') {
-        this.error = 'AreYouRobot'
-        return
-      }
-      this.$ihr_api.userResetPassword(
+      // if (this.recaptcha == '') {
+      //     this.error = 'AreYouRobot'
+      //     return
+      // }
+      this.error = null
+      this.$ihr_api.userforgetpassword(
         this.email,
         this.password,
-        this.recaptcha,
-        () => {
-          this.error = null
+        this.code,
+        res => {
+          this.emailSent = true
+          this.message = res.msg
         },
         error => {
-          this.error = error.status
-          console.log(error.detail)
+          this.emailSent = true
+          this.message = error.detail
         }
       )
     },
   },
-  computed: {
-    title() {
-      if (this.$ihr_api.authenticated) {
-        return this.$t('resetPassword.youAreLoggedIn')
-      }
-      if (this.token != undefined) {
-        return this.$t('resetPassword.fillToChange')
-      }
-      return this.emailSent ? this.$t('resetPassword.emailSent') : this.$t('resetPassword.title')
-    },
-  },
+  computed: {},
 }
 </script>
 <style lang="stylus" scoped>
