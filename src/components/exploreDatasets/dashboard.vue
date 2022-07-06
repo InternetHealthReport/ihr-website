c54405ec2eb924edba9a78c62ceb48a3bb263c08
Aniket762 <aniketindian8@gmail.com>
2022-07-06 23:45:26 +0530

    feat:added chart for mlab

--- src/components/exploreDatasets/dashboard.vue
+++ src/components/exploreDatasets/dashboard.vue
@@ -7,16 +7,17 @@
     <q-select v-model="ySelected" label="Select Y-axis" :options="yaxis"></q-select>
     <q-select v-model="yearSelected" label="Select Year" :options="years"></q-select>
     <button @click="search(yearSelected)">Search</button>
-    <div v-for="network in networks" :key="network.id">
+    <!-- <div v-for="network in networks" :key="network.id">
       <h3>{{ network.date }} - {{ network.dl_LOG_AVG_rnd1 }}</h3>
-    </div>
-    <reactive-chart :layout="layout" :traces="traces" @plotly-click="showTable" :ref="myId" :no-data="noData" :yMax="yMax" />
+    </div> -->
+    <reactive-chart :layout="layout" :traces="traces" :no-data="noData" :chartTitle="MLab" />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import ReactiveChart from '../ReactiveChart.vue'
+import { NET_DELAY_LAYOUT } from '../../views/charts/layouts'
 export default {
   name: 'Exploratory Dashboard',
   components: {
@@ -28,7 +29,10 @@ export default {
     var xaxis = ['X1', 'X2', 'X3']
     var yaxis = ['Y1', 'Y2', 'Y3']
     var years = ['2019', '2020', '2021']
-    //var layout = NET_DELAY_LAYOUT
+    // change the layout according to the map
+    var layout = {
+      title: 'Line and Scatter Plot',
+    }
     return {
       countries: countries,
       countrySelected: null,
@@ -42,24 +46,57 @@ export default {
       yearSelected: null,
       networks: [],
       errorMsg: '',
+      layout: layout,
+      traces: [],
     }
   },
   methods: {
     search(yearSelected) {
       this.getInfo(yearSelected)
+      console.log('******************SEARCH FUNCTION IS CALLED**********')
     },
     getInfo(yearSelected) {
       console.log(yearSelected)
+      console.log('******************GET INFO FUNCTION IS CALLED**********')
       axios
         .get('https://statistics.measurementlab.net/v0/NA/US/US-MD/2020/histogram_daily_stats.json')
         .then(response => {
-          console.log(response.data)
+          // console.log(response.data)
           this.networks = response.data
+          this.getChart(this.networks)
         })
         .catch(error => {
           console.log(error)
           this.errorMsg = 'MLab API end point not working'
         })
+    },
+    getChart(networks) {
+      console.log('******************GET CHART FUNCTION IS CALLED**********')
+      console.log(networks[0].date)
+      let arr1 = []
+      networks.forEach(network => {
+        arr1.push(network.date)
+      })
+      let arr2 = []
+      networks.forEach(network => {
+        arr2.push(network.bucket_max)
+      })
+      this.traces = [
+        {
+          x: arr1,
+          y: arr2,
+          mode: 'scatter',
+        },
+      ]
+      console.log(this.networks.date)
+      // this.traces = [
+      //   {
+      //     x: [1, 2, 3, 4],
+      //     y: [16, 5, 11, 9],
+      //     mode: 'scatter',
+      //   },
+      // ]
+      console.log(this.traces.x)
     },
   },
 }
