dd952c00faa4e8b7329f54a4b817e7cf8f89e678
Aniket762 <aniketindian8@gmail.com>
2022-08-09 19:38:13 +0530

    feat: timeline date changed to human readable format

--- src/views/charts/IodaChart.vue
+++ src/views/charts/IodaChart.vue
@@ -1,93 +1,99 @@
 <template>
-<div>
-    <reactive-chart 
-        :layout="layout" 
-        :traces="traces" 
-        :noData="noData" 
-        :chartTitle="iodaChart"
-    /> 
-</div>
+  <div>
+    <reactive-chart :layout="layout" :traces="traces" :noData="noData" :chartTitle="iodaChart" />
+  </div>
 </template>
 
 <script>
-import axios from "axios"
-import ReactiveChart from "../../components/ReactiveChart.vue"
+import axios from 'axios'
+import ReactiveChart from '../../components/ReactiveChart.vue'
 export default {
-    name:"Ioda Chart",
-    components:{
-        ReactiveChart
-    },
-    props:{
-        ASN:String,
-        StartTime : String,
-        EndTime : String
-    },
-    data(){
-        var layout = {
-            title:"Ioda dataset of time vs metadata access calls",
-            xaxis:{
-                title:"Timeline"
-            },
-            yaxis:{
-                title:"Metadata access"
+  name: 'Ioda Chart',
+  components: {
+    ReactiveChart,
+  },
+  props: {
+    ASN: String,
+    StartTime: String,
+    EndTime: String,
+  },
+  data() {
+    var layout = {
+      title: '',
+      xaxis: {
+        title: 'Timeline',
+      },
+      yaxis: {
+        title: 'Metadata access',
+      },
+    }
+    return {
+      layout: layout,
+      errorMsg: '',
+      traces: [],
+    }
+  },
+  methods: {
+    getInfo(ASN, StartTime, EndTime) {
+      const startDate = new Date(StartTime)
+      const startUnixTimeStamp = Math.floor(startDate.getTime() / 1000)
+      const endDate = new Date(EndTime)
+      const endUnixTimeStamp = Math.floor(endDate.getTime() / 1000)
+      axios
+        .get(
+          `https://api.ioda.inetintel.cc.gatech.edu/v2/signals/raw/asn/${ASN}?from=${startUnixTimeStamp}&until=${endUnixTimeStamp}&datasource=ping-slash24`
+        )
+        .then(response => {
+          this.networks = response.data
+          const networksData = this.networks.data
+          for (const networkLayer of networksData) {
+            for (const network of networkLayer) {
+              this.getChart(network)
+              console.log(network)
             }
-        }
-        return{
-            layout:layout,
-            errorMsg:"",
-            traces:[]
-        }
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          this.errorMsg = 'Ioda API end point is not working'
+        })
     },
-    methods:{
-        getInfo(ASN,StartTime,EndTime){
-            const startDate = new Date(StartTime);
-            const startUnixTimeStamp = Math.floor(startDate.getTime()/1000);
-            const endDate = new Date(EndTime);
-            const endUnixTimeStamp = Math.floor(endDate.getTime()/1000);
-            axios.get(`https://api.ioda.inetintel.cc.gatech.edu/v2/signals/raw/asn/${ASN}?from=${startUnixTimeStamp}&until=${endUnixTimeStamp}&datasource=ping-slash24`)
-            .then(response =>{
-                this.networks = response.data
-                const networksData = this.networks.data    
-                for(const networkLayer of networksData){
-                    for(const network of networkLayer){
-                       this.getChart(network)
-                       console.log(network)
-                    }       
-                }
-            })
-            .catch(error =>{
-                console.log(error)
-                this.errorMsg = "Ioda API end point is not working"
-            })
-        },
-        getChart(network){
-        let networkDates = []
-        let networkStart = network.from 
-        let networkEnd = network.until
-        let networkStep = network.nativeStep
-        // neglecting from and until timestamps
-        while(networkStart<networkEnd){
-            networkStart += networkStep 
-            networkDates.push(networkStart);
-        }                
-        
-        // storing network Values
-        let networkValues = network.values;
+    getChart(network) {
+      let networkDates = []
+      let networkStart = network.from
+      let networkEnd = network.until
+      let networkStep = network.nativeStep
+      // neglecting from and until timestamps
+      while (networkStart < networkEnd) {
+        networkStart += networkStep
+        networkDates.push(networkStart)
+      }
 
-        // building the trace
-        this.traces = [
-            {
-                x:networkDates,
-                y:networkValues,
-                mode:"Scatter",
-                name:"Ping-Slash 24"
-            }
-        ]
-    }
+      let readableDate = []
+      for (let unixDate in networkDates) {
+        var newDate = new Date()
+        newDate.setTime(unixDate * 1000)
+        let dateString = newDate.toUTCString()
+
+        readableDate.push(dateString)
+      }
+      console.log(readableDate)
+      // storing network Values
+      let networkValues = network.values
+      // building the trace
+      this.traces = [
+        {
+          x: readableDate,
+          y: networkValues,
+          mode: 'Scatter',
+          name: 'Ping-Slash 24',
+        },
+      ]
     },
-    mounted:function(){
-        this.getInfo(this.ASN,this.StartTime,this.EndTime);
-    }
+  },
+  mounted: function () {
+    this.getInfo(this.ASN, this.StartTime, this.EndTime)
+  },
 }
 </script>
 
