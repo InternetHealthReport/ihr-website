915f8ef7c567780389e84c7056d81e36bf268e79
Aniket762 <aniketindian8@gmail.com>
2022-09-04 15:02:30 +0530

    feat:type of plot search added

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <h1>Dashboard</h1>
+    <h1 class="text-center">Explorer</h1>
+    <div class="row justify-center">
+      <div class="col-3">
+        <q-select v-model="selectedPlot" :options="plots" label="Type of Plot" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Exploratory Dashboard',
+  data() {
+    var plots = ['Network Delay', 'IODA Rechability of /24s (%)', 'NDT Speed Test']
+    return {
+      plots: plots,
+      selectedPlot: null,
+    }
+  },
 }
 </script>
