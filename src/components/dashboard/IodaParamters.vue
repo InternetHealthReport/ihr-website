6c8b02a7836c8237d064f2cc3fcce28e4204df28
Aniket762 <aniketindian8@gmail.com>
2022-09-24 09:56:19 +0530

    multiple charts on ioda added

--- src/components/dashboard/IodaParamters.vue
+++ src/components/dashboard/IodaParamters.vue
@@ -5,11 +5,13 @@
       <network-search-bar bg="white" label="grey-8" input="black" labelTxt="Enter the ASN you are looking for" />
       <div class="q-pa-md">
         <div class="q-pb-sm">Model: {{ dateRange }}</div>
-        <q-card class="IHR_charts-body">
-          <q-card-section v-if="dateRange">
-            <ioda-chart :ASN="asNumber" :fetch="true" :start-time="getFrom(dateRange)" :end-time="getTo(dateRange)" ref="iodaChart" />
-          </q-card-section>
-        </q-card>
+        <div v-for="dateRange in iodaChartArray" :key="dateRange">
+          <q-card class="IHR_charts-body">
+            <q-card-section v-if="dateRange">
+              <ioda-chart :ASN="asNumber" :fetch="true" :start-time="getFrom(dateRange)" :end-time="getTo(dateRange)" ref="iodaChart" />
+            </q-card-section>
+          </q-card>
+        </div>
         <q-date v-model="dateRange" range />
       </div>
     </div>
@@ -31,9 +33,11 @@ export default {
   data() {
     let dateRange
     let asNumber = '2497'
+    let iodaChartArray = []
     return {
       dateRange: dateRange,
       asNumber: asNumber,
+      iodaChartArray: iodaChartArray,
     }
   },
   methods: {
@@ -44,6 +48,11 @@ export default {
     getTo(dateRange) {
       let to = new Date(dateRange.to)
       return to
+    },
+  },
+  watch: {
+    dateRange() {
+      this.iodaChartArray.push(this.dateRange)
     },
   },
 }
