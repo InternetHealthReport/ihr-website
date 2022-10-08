6a65a9310191ada203bd9ee1c6d8c609d03a0974
Aniket762 <aniketindian8@gmail.com>
2022-10-08 16:55:42 +0530

    feat:added add button to append charts

--- src/components/dashboard/IodaParamters.vue
+++ src/components/dashboard/IodaParamters.vue
@@ -2,17 +2,27 @@
   <div class="row justify-center">
     <h1>IODA plots will appear here</h1>
     <div class="col-12">
-      <network-search-bar bg="white" label="grey-8" input="black" labelTxt="Enter the ASN you are looking for" />
+      <div class="row justify-center">
+        <div class="col-5">
+          <network-search-bar bg="white" label="grey-8" input="black" labelTxt="Enter the ASN you are looking for" />
+        </div>
+        <div class="col-5">
+          <q-date v-model="dateRange" range />
+        </div>
+        <div class="col-2">
+          <button @click="getParam()">Add plot</button>
+        </div>
+      </div>
       <div class="q-pa-md">
         <div class="q-pb-sm">Model: {{ dateRange }}</div>
         <div v-for="dateRange in iodaChartArray" :key="dateRange">
           <q-card class="IHR_charts-body">
             <q-card-section v-if="dateRange">
+              <h1>{{ asNumber }}</h1>
               <ioda-chart :ASN="asNumber" :fetch="true" :start-time="getFrom(dateRange)" :end-time="getTo(dateRange)" ref="iodaChart" />
             </q-card-section>
           </q-card>
         </div>
-        <q-date v-model="dateRange" range />
       </div>
     </div>
   </div>
@@ -49,9 +59,8 @@ export default {
       let to = new Date(dateRange.to)
       return to
     },
-  },
-  watch: {
-    dateRange() {
+    getParam() {
+      console.log('HEHE')
       this.iodaChartArray.push(this.dateRange)
     },
   },
