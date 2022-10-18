da21c7dd203059aca0ed2586a3f5d6e2b736936c
Aniket762 <aniketindian8@gmail.com>
2022-10-18 11:17:02 +0530

    feat:mlab chart initiated in dashboard

--- src/components/dashboard/MlabParameters.vue
+++ src/components/dashboard/MlabParameters.vue
@@ -1,5 +1,71 @@
 <template>
   <div class="row justify-center">
-    <h1>Measurement Lab plots will appear here</h1>
+    <h1>Mlab plots will appear here</h1>
+    <div class="col-12">
+      <div class="row justify-center">
+        <div class="col-5">
+          <network-search-bar bg="white" label="grey-8" input="black" labelTxt="Enter the ASN you are looking for" />
+        </div>
+        <div class="col-5">
+          <q-date v-model="YearPicker" />
+        </div>
+        <div class="col-2">
+          <button @click="addPlot()">Add Plot</button>
+        </div>
+      </div>
+      <div class="q-pa-md">
+        <div class="q-pb-sm">Model:{{ YearPicker }}</div>
+        <div v-for="index in mlabChartArray.length" :key="index">
+          <q-card class="IHR_charts-body">
+            <q-card-section v-if="mlabChartArray[index]">
+              <h1>{{ asNumber }}</h1>
+              <h1 @click="deletePlot(index)">X</h1>
+              <measurement-lab
+                :start-time="getFrom(mlabChartArray[index])"
+                :end-time="getFrom(mlabChartArray[index])"
+                :ASN="asNumber"
+                :fetch="fetch"
+                ref="measurementLabChart"
+              />
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import NetworkSearchBar from './middleware/networkSearchBar.vue'
+import YearPicker from './middleware/yearPicker.vue'
+import MeasurementLab from './charts/MlabChart.vue'
+export default {
+  components: {
+    NetworkSearchBar,
+    YearPicker,
+    MeasurementLab,
+  },
+  data() {
+    let YearPicker
+    let asNumber = '2497'
+    let mlabChartArray = []
+    return {
+      asNumber: asNumber,
+      YearPicker: YearPicker,
+      mlabChartArray: mlabChartArray,
+    }
+  },
+  methods: {
+    getYear(YearPicker) {
+      let year = new Date(YearPicker.year)
+      return year
+    },
+    addPlot() {
+      this.mlabChartArray.push(this.YearPicker)
+    },
+    deletePlot(index) {
+      this.mlabChartArray.splice(index)
+    },
+  },
+}
+</script>
---
+++ src/components/dashboard/middleware/yearPicker.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-pb-sm">Model: {{ model }}</div>
+
+    <q-date v-model="model" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    return {
+      model: ref({ year: '2020/07/08' }),
+    }
+  },
+}
+</script>
