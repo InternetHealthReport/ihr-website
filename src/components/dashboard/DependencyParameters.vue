1047bc88bb826cabc995d2cfad5432ba83e314b0
Aniket762 <aniketindian8@gmail.com>
2022-11-09 11:23:16 +0530

    feat:network search daterange added dependency dashboard

--- src/components/dashboard/DependencyParameters.vue
+++ src/components/dashboard/DependencyParameters.vue
@@ -1,11 +1,261 @@
 <template>
-  <div>
-    <h1>AS Interdepency Matrix</h1>
+  <div class="row justify-center">
+    <h1>AS Interdependency Matrix</h1>
+    <div class="col-12">
+      <div class="row justify-center">
+        <!-- Search Bar Start -->
+        <div class="searchbar_div">
+          <div class="Subscribe">
+            <p v-if="tags.length == 0" class="IHR_description">Select Networks</p>
+            <div v-else class="tag">
+              <p class="IHR_description">Selected Networks:</p>
+              <el-tag v-for="(item, index) in tags" :key="index" type="warning" style="margin: 5px 8px" @close="handleClose(item)" closable>
+                {{ item.channel.split(',')[0] }}
+              </el-tag>
+            </div>
+          </div>
+
+          <div class="select">
+            <q-btn-toggle
+              v-model="panel"
+              rounded
+              @click="changePanel(panel)"
+              toggle-color="blue"
+              no-caps
+              :options="[{ label: 'networks', value: 'network' }]"
+            />
+            <search-bar class="col-3 q-px-sm" :type="panel" @searchRes="searchChange" style="margin: 20px 0" />
+            <q-tab-panels v-model="panel" animated style="border-top: 1px solid #ccc">
+              <q-tab-panel keep-alive name="network">
+                <div class="btn_list">
+                  <q-btn
+                    outline
+                    v-for="(item, index) in dataList"
+                    :key="index"
+                    color="white"
+                    style="width: 175px !important"
+                    text-color="black"
+                    :label="item.split(',')[0]"
+                    @click="select(item)"
+                    no-caps
+                  >
+                    <q-tooltip class="bg-accent">{{ item }}</q-tooltip>
+                  </q-btn>
+                </div>
+              </q-tab-panel>
+            </q-tab-panels>
+          </div>
+        </div>
+        <!-- Search Bar End -->
+        <div class="col-5">
+          <q-date v-model="dateRange" range />
+          <p>{{ dateRange }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import searchBar from './middleware/searchBar.vue'
 export default {
-  name: 'DependencyParameters',
+  components: {
+    searchBar,
+  },
+  data() {
+    let dateRange
+    return {
+      dateRange: dateRange,
+      tags: [],
+      asNumberArray: [],
+      panel: 'network',
+      word: '',
+      dataList: [],
+      network: [
+        'AS3356 - Lumen',
+        'AS2914 - NTT',
+        'AS6939 - HE',
+        'AS1299 - Telia',
+        'AS174  - Cogent',
+        'AS15169 - Google',
+        'AS20940 - Akamai',
+        'AS16509 - Amazon',
+        'AS13335 - Cloudflare',
+        'AS32934 - Facebook',
+        'AS7922  - Comcast',
+        'AS8075  - Microsoft',
+      ],
+    }
+  },
+  mounted() {
+    this.oldChannel()
+    this.dataList = this.country
+  },
+  methods: {
+    getFrom(dateRange) {
+      let from = new Date(dateRange.from)
+      return from
+    },
+    getTo(dateRange) {
+      let to = new Date(dateRange.to)
+      return to
+    },
+    getASN(tagNumber) {
+      let ASN = tagNumber.substring(2, tagNumber.indexOf(' '))
+      return ASN
+    },
+    addPlot() {
+      this.iodaChartArray.push(this.dateRange)
+    },
+    deletePlot(index) {
+      this.iodaChartArray.splice(index)
+    },
+    oldChannel() {
+      this.$ihr_api.getChannel(
+        res => {
+          console.log(res)
+          if (res.hasOwnProperty('data')) {
+            this.tags = res.data.channel
+          }
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    },
+    select(label) {
+      let flag = true
+      flag = this.tags.find(item => item.channel === label)
+      if (!flag) {
+        this.tags.push({ channel: label, frequency: 'normal' })
+      }
+    },
+    handleClose(tag) {
+      this.tags.splice(this.tags.indexOf(tag), 1)
+    },
+    searchChange(data) {
+      if (data) {
+        this.dataList = [data]
+      } else {
+        switch (this.panel) {
+          case 'country':
+            this.dataList = this.country
+            break
+          case 'city':
+            this.dataList = this.city
+            break
+          case 'network':
+            this.dataList = this.network
+            break
+        }
+      }
+    },
+    changePanel(val) {
+      this.word = ''
+      console.log(val)
+      switch (val) {
+        case 'country':
+          this.dataList = this.country
+          break
+        case 'city':
+          this.dataList = this.city
+          break
+        case 'network':
+          this.dataList = this.network
+          break
+      }
+    },
+  },
 }
 </script>
+
+<style lang="stylus">
+@import '../../styles/quasar.variables';
+.searchbar_div {
+  width: 60%;
+  margin: 0 auto;
+}
+.Subscribe {
+  position: relative;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #ccc;
+}
+
+.IHR_description {
+  font-size: 20px;
+  color: #d6d6d6;
+}
+
+.subbnt {
+  position: absolute;
+  height: 38px;
+  bottom: 25px;
+  right: 0;
+}
+
+.group_select {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: flex-end;
+  position: absolute;
+  height: 100px;
+  width: 240px;
+  bottom: 25px;
+  right: 0;
+}
+
+.group_select .q-btn {
+  width: 100px;
+  height: 38px;
+}
+
+.select {
+  margin-top: 20px;
+}
+
+.select .q-btn {
+  width: 102px;
+}
+
+.select .q-field__control {
+  height: 40px;
+}
+
+.select .q-field__label {
+  line-height: 10px;
+}
+
+.select .q-field__marginal {
+  height: 40px;
+}
+
+.btn_list {
+  height: 150px;
+  display: flex;
+  justify-content: flex-start;
+  align-content: flex-start;
+  flex-wrap: wrap;
+}
+
+.btn_list .q-btn {
+  margin: 10px 5px;
+  overflow: hidden;
+}
+
+.tag {
+  display: flex;
+  justify-content: flex-start;
+  align-content: space-between;
+  flex-wrap: wrap;
+  width: 78%;
+  min-height: 46px;
+}
+
+.el-tag {
+  text-align: center !important;
+  height: 36px !important;
+  line-height: 36px !important;
+  font-size: 16px !important;
+}
+</style>
