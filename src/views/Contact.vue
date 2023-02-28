039e6ba1ebb6926b03c08534af9f3123c6bced98
Anurag Biswal <anugbswl@gmail.com>
2023-02-28 19:29:14 +0530

    Fix:removed the IHR logo from contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,27 +1,18 @@
 <template>
-  <div>
-    <div id="IHR_contact-page">
-      <div class="IHR_contact-page">
-        <h1>Feedback</h1>
-        <p class="IHR_description">
-          Please submit bug reports and feature requests on Github:
-          <a href="https://github.com/InternetHealthReport/ihr-website/issues" target="_blank"
-            >https://github.com/InternetHealthReport/ihr-website/issues</a
-          >
-        </p>
-        <h1>Others</h1>
-        <p>
-          For questions and inquiries, please contact us at:
-          <a href="mailto:admin@ihr.live">admin@ihr.live</a>
-        </p>
-      </div>
+  <div id="IHR_contact-page">
+    <div class="IHR_contact-page">
+      <h1>Feedback</h1>
+      <p class="IHR_description">
+        Please submit bug reports and feature requests on Github:
+        <a href="https://github.com/InternetHealthReport/ihr-website/issues"
+          target="_blank">https://github.com/InternetHealthReport/ihr-website/issues</a>
+      </p>
+      <h1>Others</h1>
+      <p>
+        For questions and inquiries, please contact us at:
+        <a href="mailto:admin@ihr.live">admin@ihr.live</a>
+      </p>
     </div>
-    <div
-      class="IHR_background"
-      :style="{
-        backgroundImage: 'url(' + require('@/assets/imgs/ihr_logo.svg') + ')',
-      }"
-    ></div>
   </div>
 </template>
 <script>
