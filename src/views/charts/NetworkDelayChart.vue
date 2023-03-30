16d8cb3fdc0f3422469f8bce9781776de712c332
Kartik Kankurte <kartikkankurte@gmail.com>
2023-03-30 20:34:54 +0530

    Fixed#376: styling of input and placeholders

--- src/views/charts/NetworkDelayChart.vue
+++ src/views/charts/NetworkDelayChart.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="IHR_chart">
-    <div class="row justify-center" v-if="searchBar">
-      <div class="col-4 q-pa-sm">
+    <div class="justify-center" v-if="searchBar">
+      <div>
+      <div class="q-pa-sm"
+      >
         <location-search-bar
           @select="addStartLocation"
           :hint="$t('searchBar.locationSource')"
           :label="$t('searchBar.locationHint')"
           :selected="startPointNameStr"
+          style="width: 65%;margin: auto; margin-bottom: -6px;"
+          />
+      </div>
+      <div class="q-pa-sm"
+      >
+        <location-search-bar 
+        @select="addEndLocation" 
+        :hint="$t('searchBar.locationDestination')" 
+        :label="$t('searchBar.locationHint')" 
+        :selected="startPointNameStr"
+        style="width: 65%;margin: auto;margin-bottom: -6px;"
         />
       </div>
-      <div class="col-4 q-pa-sm">
-        <location-search-bar @select="addEndLocation" :hint="$t('searchBar.locationDestination')" :label="$t('searchBar.locationHint')" />
       </div>
+      <div style="display: block;">
       <div class="col-3 q-pa-sm">
         <q-btn @click="debouncedApiCall" color="secondary" class="q-ml-sm">Add</q-btn>
         <q-btn @click="clearGraph" class="q-ml-sm">Clear all</q-btn>
+      </div>
       </div>
     </div>
     <div class="row">
