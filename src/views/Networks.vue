f35c20e2c511358ebeab3aaa729eb7894e309b7c
Arnab Ghosh <arnabsonu2@gmail.com>
2024-02-29 08:45:10 +0530

    Fixed Examples In Network and Countries page (#713)

--- src/views/Networks.vue
+++ src/views/Networks.vue
@@ -307,15 +307,15 @@ onMounted(() => {
           </div>
         </div>
       </div>
-      <div class="q-pa-xl">
-        <div class="row justify-center">
+      <div class="q-pa-lg">
+        <div class="row q-pa-lg column items-center">
           <div class="col-6">
             <h3>Examples:</h3>
           </div>
         </div>
         <div class="row justify-center">
-          <div class="col-3">
-            <ul>
+          <div class="row examples">
+            <ul class="ul_styles">
               <li>
                 <RouterLink :to="Tr.i18nRoute({ name: 'networks', params: { asn: 'AS2497' } })" class="IHR_delikify">IIJ (AS2497)</RouterLink>
               </li>
@@ -326,9 +326,7 @@ onMounted(() => {
                 <RouterLink :to="Tr.i18nRoute({ name: 'networks', params: { asn: 'AS2501' } })" class="IHR_delikify">University of Tokyo (AS2501)</RouterLink>
               </li>
             </ul>
-          </div>
-          <div class="col-3">
-            <ul>
+            <ul class="ul_styles">
               <li>
                 <RouterLink :to="Tr.i18nRoute({ name: 'networks', params: { asn: 'AS7922' } })" class="IHR_delikify">Comcast (AS7922)</RouterLink>
               </li>
@@ -351,4 +349,13 @@ onMounted(() => {
   &char-container
     width 90%
     margin 0 auto
+.examples
+  column-gap 30px
+@media screen and (max-width: 500px)
+  .examples
+    flex-direction column
+.ul_styles
+  padding 0
+  margin 0
+  list-style-position: inside
 </style>
