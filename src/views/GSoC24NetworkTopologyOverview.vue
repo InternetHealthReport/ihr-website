cf0122a9117fe09c93a474c0b0de2ff8cc9a3bb4
0xAnon <utkarshshail005@protonmail.com>
2024-06-01 04:29:14 +0530

    Network Topology Basic UI Init

--- src/views/GSoC24NetworkTopologyOverview.vue
+++ src/views/GSoC24NetworkTopologyOverview.vue
@@ -1,8 +1,34 @@
 <script setup>
+import { QBtn, QSelect, QInput } from 'quasar'
+import { ref } from 'vue' 
+
+const ipOptions = ref(['IPv4', 'IPv6'])
+const ipModel = ref('IPv4')
+const asn = ref('')
+
 </script>
 
 <template>
-	[GSoC24] Network topology overview
+
+	<div>
+	
+		<h1 class="text-center q-pa-xl">Network Topology Overview</h1>
+
+		<div class="justify-center q-pa-md flex pb-0">
+			<QInput style="max-width: 120px"  outlined v-model="asn" placeholder="ASN" :dense="true" />
+			<QSelect
+        	style="min-width: 100px; margin-left: 22px;"
+			filled
+			v-model="ipModel"
+			:options="ipOptions"
+			label="IP"
+			:dense="true"
+      		/>
+			<QBtn style="margin-left:22px" color="secondary" label="Search" />
+		</div>
+
+	</div>
+
 </template>
 
 <style lang="stylus" scoped>
