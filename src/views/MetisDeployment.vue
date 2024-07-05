InternetHealthReport/ihr-website

commit c3eb1d1002dcb535f78a38f8150df7dadb1f5da0

Fix cache in errors and timeouts & change routes (#774)
* Fix cache in errors and timeouts

* Increase package number

* Remove OR from home page

* Fix search bar buttons

* Fix line in home page

* Made search async

* Add async loading

* Rename router for prefixes: From networks to prefixes

* fix routers: from plural to singular

* fix redirection from old website

src/views/MetisDeployment.vue
@@ -19,7 +19,7 @@ const nbprobes = ref('100')
         </p>
         <p>
           The recommendations are updated every Sunday/Monday night at midnight UTC. For historical queries, or more recommendations, check
-          out the <router-link to="/en-us/api">Metis section of the IHR API</router-link>.
+          out the <router-link :to="Tr.i18nRoute({ name: 'api'})">Metis section of the IHR API</router-link>.
         </p>
       </div>
     </div>

index.html
@@ -18,7 +18,7 @@
       content="Internet Health Report | Monitoring networks health"
     />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://ihr.iijlab.net/ihr/en-us/" />
+    <meta property="og:url" content="https://ihr.iijlab.net/ihr/" />
     <meta property="og:site_name" content="Internet Health Report" />
     <meta
       property="og:title"

src/components/Footer.vue
@@ -50,19 +50,19 @@ import { version } from '../../package.json'
               <RouterLink :to="Tr.i18nRoute({ name: 'global-report' })">{{ $t('footer.reportPages.global') }}</RouterLink>
             </li>
             <li>
-              <RouterLink :to="Tr.i18nRoute({ name: 'countries', params: { cc: null } })">{{ $t('footer.reportPages.country') }}</RouterLink>
+              <RouterLink :to="Tr.i18nRoute({ name: 'country', params: { cc: null } })">{{ $t('footer.reportPages.country') }}</RouterLink>
             </li>
             <li>
-              <RouterLink :to="Tr.i18nRoute({ name: 'networks', params: { id: null } })">{{ $t('footer.reportPages.network') }}</RouterLink>
+              <RouterLink :to="Tr.i18nRoute({ name: 'network', params: { id: null } })">{{ $t('footer.reportPages.network') }}</RouterLink>
             </li>
             <li>
-              <RouterLink :to="Tr.i18nRoute({ name: 'hostnames', params: { id: null } })">{{ $t('footer.reportPages.hostName') }}</RouterLink>
+              <RouterLink :to="Tr.i18nRoute({ name: 'hostname', params: { id: null } })">{{ $t('footer.reportPages.hostName') }}</RouterLink>
             </li>
             <li>
-              <RouterLink :to="Tr.i18nRoute({ name: 'tags', params: { id: null } })">{{ $t('footer.reportPages.tag') }}</RouterLink>
+              <RouterLink :to="Tr.i18nRoute({ name: 'tag', params: { id: null } })">{{ $t('footer.reportPages.tag') }}</RouterLink>
             </li>
             <li>
-              <RouterLink :to="Tr.i18nRoute({ name: 'ranks', params: { rank: null } })">{{ $t('footer.reportPages.rank') }}</RouterLink>
+              <RouterLink :to="Tr.i18nRoute({ name: 'rank', params: { rank: null } })">{{ $t('footer.reportPages.rank') }}</RouterLink>
             </li>
             <li>
               <RouterLink :to="Tr.i18nRoute({ name: 'rov' })">{{ $t('footer.reportPages.rov') }}</RouterLink>

src/components/Header.vue
@@ -25,27 +25,27 @@ const  SIMPLE_MENU = [
       },
       {
         entryName: 'header.countryReport.name',
-        routeName: 'countries',
+        routeName: 'country',
         summary: 'header.countryReport.summary',
       },
       {
         entryName: 'header.networkReport.name',
-        routeName: 'networks',
+        routeName: 'network',
         summary: 'header.networkReport.summary',
       },
       {
         entryName: 'header.hostNameReport.name',
-        routeName: 'hostnames',
+        routeName: 'hostname',
         summary: 'header.hostNameReport.summary',
       },
       {
         entryName: 'header.tagReport.name',
-        routeName: 'tags',
+        routeName: 'tag',
         summary: 'header.tagReport.summary',
       },
       {
         entryName: 'header.rankReport.name',
-        routeName: 'ranks',
+        routeName: 'rank',
         summary: 'header.rankReport.summary',
       },
       {

src/components/UserInfo.vue
@@ -52,7 +52,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <QMarkupTable class="card" dense bordered>
+  <QMarkupTable class="user-info-card" dense bordered>
     <thead>
       <th :colspan="2" align="center" style="border-bottom: 1px solid white;">
         <td style="font-size: large;">YOUR CONNECTION</td>
@@ -60,28 +60,28 @@ onMounted(() => {
     </thead>
     <tbody v-if="!as_info_query.loading">
       <tr>
-        <td align="right" class="text">IP:</td>
-        <td align="left" class="text">{{userInfo.IP}}</td>
+        <td align="right" class="user-info-text">IP:</td>
+        <td align="left" class="user-info-text">{{userInfo.IP}}</td>
       </tr>
       <tr>
-        <td align="right" class="text">AS:</td>
-        <td align="left" class="text">
-          <RouterLink :to="Tr.i18nRoute({ name: 'networks', params: { id: `AS${userInfo.AS}` } })" class="link">
+        <td align="right" class="user-info-text">AS:</td>
+        <td align="left" class="user-info-text">
+          <RouterLink :to="Tr.i18nRoute({ name: 'network', params: { id: `AS${userInfo.AS}` } })" class="user-info-link">
           AS{{userInfo.AS}} - {{userInfo.AS_NAME}}
           </RouterLink>
         </td>
       </tr>
       <tr>
-        <td align="right" class="text">PREFIX:</td>
-        <td align="left" class="text"><RouterLink :to="Tr.i18nRoute({ name: 'networks', params: { id: userInfo.PREFIX.split('/')[0], length: userInfo.PREFIX.split('/')[1] } })" class="link">
+        <td align="right" class="user-info-text">PREFIX:</td>
+        <td align="left" class="user-info-text"><RouterLink :to="Tr.i18nRoute({ name: 'prefix', params: { ip: userInfo.PREFIX.split('/')[0], length: userInfo.PREFIX.split('/')[1] } })" class="user-info-link">
           {{userInfo.PREFIX}}
           </RouterLink>
         </td>
       </tr>
       <tr>
-        <td align="right" class="text">COUNTRY:</td>
-        <td align="left" class="text">
-          <RouterLink :to="Tr.i18nRoute({ name: 'countries', params: { cc: userInfo.CC } })" class="link">
+        <td align="right" class="user-info-text">COUNTRY:</td>
+        <td align="left" class="user-info-text">
+          <RouterLink :to="Tr.i18nRoute({ name: 'country', params: { cc: userInfo.CC } })" class="user-info-link">
             {{userInfo.COUNTRY}}
           </RouterLink>
         </td>
@@ -94,17 +94,17 @@ onMounted(() => {
 </template>
 
 <style lang="stylus">
-.card
+.user-info-card
   width inherit
   background-color rgba(0,0,0,0) !important
   border-color white !important
   color white !important
 
-.text
+.user-info-text
   font-size 14px !important
   font-weight 500 !important
 
-.link
+.user-info-link
   color white !important
 
 .loading-spinner

src/components/iyp/as/ASOriginatedPrefixes.vue
@@ -70,7 +70,7 @@ onMounted(() => {
         <IypGenericPieChart v-if="prefixes.data.length > 0" :chart-data="prefixes.data" :chart-layout="{ title: 'Geo-location (Maxmind)' }" />
       </div>
       <div class="col-6">
-        <IypGenericBarChart v-if="prefixes.data.length > 0" :chart-data="prefixes.data" :config="{key:'tags'}" :chart-layout="{ title: 'Tags' }" />
+        <IypGenericBarChart v-if="prefixes.data.length > 0" :chart-data="prefixes.data" :config="{key:'tag'}" :chart-layout="{ title: 'tag' }" />
       </div>
       <div class="col-10">
       <IypGenericTreemapChart

src/components/iyp/country/CountryIPPrefixes.vue
@@ -95,8 +95,8 @@ onMounted(() => {
         <IypGenericBarChart
           v-if="prefixes.data.length > 0"
           :chart-data="prefixes.data"
-          :config="{key:'tags'}"
-          :chart-layout="{ title: 'Tags' }"
+          :config="{key:'tag'}"
+          :chart-layout="{ title: 'tag' }"
         />
       </div>
       <div class="col-8">

src/components/iyp/hostName/HostNameAuthoritativeNameservers.vue
@@ -64,7 +64,7 @@ onMounted(() => {
         <IypGenericBarChart
           v-if="nameservers.data.length > 0"
           :chart-data="nameservers.data"
-          :config="{key:'tags'}"
+          :config="{key:'tag'}"
           :chart-layout="{ title: 'Prefix Tags' }"
         />
       </div>

commit d2d262755f4a31745bd9d5c01564358e2786b18f

Fix Metis deployment page
Missing import caused the page to not be displayed.
Probably a regression from c3eb1d1.

Fixes #815.

src/views/MetisDeployment.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import MetisWidget from '@/components/MetisWidget.vue'
+import Tr from '@/i18n/translation'
 
 const nbprobes = ref('100')
 </script>