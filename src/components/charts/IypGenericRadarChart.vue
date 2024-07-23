f05d1a59262f4ca076d227640fcd6c17527bcf0a
Dimitrios Panteleimon Giakatos <36899728+dpgiakatos@users.noreply.github.com>
2024-07-23 18:44:54 +0900

    Replace the current ranking chart with radar chart (#824)
    * Replace Indicator with Radar chart
    
    * Add name to trace
    
    * Update info text from top three to top ten

---
+++ src/components/charts/IypGenericRadarChart.vue
@@ -0,0 +1,78 @@
+<script setup>
+import ReactiveChart from './ReactiveChart.vue'
+import { ref, onMounted, watch } from 'vue'
+
+const props = defineProps({
+  chartData: {
+    type: Array,
+    default: () => [],
+  },
+  chartLayout: {
+    type: Object,
+    default: () => ({}),
+  },
+  config: {
+    type: Object,
+    default: () => ({}),
+  }
+})
+
+const localChartData = ref([])
+const actualChartData = ref([])
+const actualChartLayout = ref({})
+
+const renderChart = () => {
+  let data = formatChartData(localChartData.value)
+
+  let layout = {
+    ...props.chartLayout,
+  }
+
+  actualChartData.value = data
+  actualChartLayout.value = layout
+}
+
+const formatChartData = (arrayOfObjects) => {
+  if (!arrayOfObjects || arrayOfObjects.length === 0) {
+    return []
+  }
+
+  let data = []
+  if (arrayOfObjects.length) {
+    data = [
+      {
+        type: 'scatterpolar',
+        r: arrayOfObjects.map(val => val.r),
+        theta: arrayOfObjects.map(val => val.theta),
+        fill: 'toself',
+        ...props.config
+      }
+    ]
+  }
+
+  return data
+}
+
+const init = () => {
+  if (props.chartData && props.chartData.length > 0) {
+    localChartData.value = props.chartData
+    renderChart()
+  }
+}
+
+watch(() => props.chartData, () => {
+  init()
+}, { deep: true })
+
+onMounted(() => {
+  init()
+})
+</script>
+
+<template>
+  <ReactiveChart
+    :layout="actualChartLayout"
+    :traces="actualChartData"
+    :chart-title="actualChartLayout && actualChartLayout.title"
+  />
+</template>
