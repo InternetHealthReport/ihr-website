b6a63b68fa263352a74b98f60217e56d5d4a9a06
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-03-25 16:24:15 +0900

    add plots for international IXP plot

--- src/components/iyp/country/CountryInternetExchangePointsInternationalDistribution.vue
+++ src/components/iyp/country/CountryInternetExchangePointsInternationalDistribution.vue
@@ -2,6 +2,9 @@
 import { useRoute, useRouter } from 'vue-router'
 import { ref, inject, watch, onMounted } from 'vue'
 import IypGenericTable from '@/components/tables/IypGenericTable.vue'
+import IypGenericBarChart from '@/components/charts/IypGenericBarChart.vue'
+import IypGenericHeatmapChart from '@/components/charts/IypGenericHeatmapChart.vue'
+import { QSlider, QBadge } from 'quasar'
 
 const iyp_api = inject('iyp_api')
 
@@ -12,6 +15,7 @@ const router = useRouter()
 
 const ixps = ref({
   data: [],
+  group: {},
   show: false,
   loading: true,
   query: `MATCH (member:AS)-[mem:MEMBER_OF]-(ix:IXP)-[:COUNTRY]-(ix_country:Country {country_code:$country_code})
@@ -83,6 +87,9 @@ const ixps = ref({
     descending: true //boolean
   }
 })
+const uniqueASperIXP = ref(6)
+const uniqueASperIXPMax = ref(0)
+const uniqueASperIXPMin = ref(0)
 
 const load = () => {
   ixps.value.loading = true
@@ -90,8 +97,68 @@ const load = () => {
   let query_params = { country_code: props.countryCode }
   iyp_api.run([{ statement: ixps.value.query, parameters: query_params }]).then((results) => {
     ixps.value.data = results[0]
+    ixps.value.group = results[0].reduce((acc, current) => {
+      if (current.ix_name) {
+        const key = `${current.ix_name.toLowerCase()} - ${current.ix_country}`
+        if (!acc[key]) {
+          acc[key] = new Set()
+        }
+        if (current.asn) {
+          acc[key].add(current.asn)
+        }
+      }
+      return acc
+    }, {})
+    const ixpsSize = Object.values(ixps.value.group).map(obj => obj.size).sort((a, b) => b - a)
+    uniqueASperIXPMax.value = ixpsSize[0]
+    const uniqueIxpSize = new Set(ixpsSize)
+    if (uniqueIxpSize.size > 5) {
+      uniqueASperIXP.value = Array.from(uniqueIxpSize)[4]
+    } else {
+      uniqueASperIXP.value = Array.from(uniqueIxpSize)[uniqueIxpSize.size - 1]
+    }
     ixps.value.loading = false
   })
+}
+
+const barPlotDataFormat = (data) => {
+  const filteredIXPs = new Set()
+  Object.keys(ixps.value.group).forEach(ixp => {
+    if (ixps.value.group[ixp].size >= uniqueASperIXP.value) {
+      filteredIXPs.add(ixp.split(' - ')[0])
+    }
+  })
+
+  const countryToASN = {}
+  data.forEach(obj => {
+    if (filteredIXPs.has(obj.ix_name.toLowerCase())) {
+      if (!countryToASN[obj.as_country]) {
+        countryToASN[obj.as_country] = new Set()
+      }
+      countryToASN[obj.as_country].add(obj.asn)
+    }
+  })
+
+  const asnCountry = []
+  Object.keys(countryToASN).forEach(cc => {
+    countryToASN[cc].forEach(_ => {
+      asnCountry.push({
+        as_country: cc
+      })
+    })
+  })
+
+  return asnCountry
+}
+
+const heatmapPlotDataFormat= (data) => {
+  const ixpDistribution = {}
+  Object.keys(data).forEach(ixp => {
+    if (data[ixp].size >= uniqueASperIXP.value) {
+      ixpDistribution[ixp] = data[ixp]
+    }
+  })
+  return [ixpDistribution]
 }
 
 watch(
@@ -116,7 +183,24 @@ onMounted(() => {
     :slot-length="1"
   >
     <div class="col-6">
-      
+      <div>
+        <h3>Filtering</h3>
+        <QBadge>Number of unique ASes per IXP: {{ uniqueASperIXP }}</QBadge>
+        <QSlider v-model="uniqueASperIXP" :min="uniqueASperIXPMin" :max="uniqueASperIXPMax" />
+      </div>
+      <IypGenericBarChart
+        v-if="ixps.data.length > 0"
+        :chart-data="barPlotDataFormat(ixps.data)"
+        :chart-layout="{ title: 'Top countries where ASNs peer (nb. unique ASNs)', yaxis: { title: { text: 'Number of peers' } } }"
+        :config="{ key: 'as_country' }"
+        :group-top-n-and-except-as-others="5"
+      />
+      <IypGenericHeatmapChart
+        v-if="ixps.data.length > 0"
+        :chart-data="heatmapPlotDataFormat(ixps.group)"
+        :chart-layout="{ title: 'IXPs distribution', xaxis: { automargin: true }, yaxis: { automargin: true } }"
+        :config="{ }"
+      />
     </div>
   </IypGenericTable>
 </template>
