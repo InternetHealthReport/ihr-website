16ad7921685f5f6d946854c9b0fecf3d46409bba
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-04-29 18:53:31 +0900

    fix bug in AS Hegemony details mechanism in plotly

--- src/components/charts/ReactiveChart.vue
+++ src/components/charts/ReactiveChart.vue
@@ -181,25 +181,6 @@ const colorPalettesScaling = {
   }
 }
 
-const addCustomModebarButton = () => {
-  const graphDiv = myId.value
-  if (!graphDiv) return
-  Plotly.newPlot(graphDiv, props.traces, layoutLocal.value, {
-    modeBarButtonsToAdd: [
-      {
-        name: 'cvd-dropdown',
-        title: 'CVD Palette',
-        icon: {
-          svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M512 256c0 .9 0 1.8 0 2.7c-.4 36.5-33.6 61.3-70.1 61.3L344 320c-26.5 0-48 21.5-48 48c0 3.4 .4 6.7 1 9.9c2.1 10.2 6.5 20 10.8 29.9c6.1 13.8 12.1 27.5 12.1 42c0 31.8-21.6 60.7-53.4 62c-3.5 .1-7 .2-10.6 .2C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm0-96a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM288 96a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm96 96a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>'
-        },
-        click: () => {
-          dropdownCVD.value = !dropdownCVD.value
-        }
-      }
-    ]
-  })
-}
-
 const updateColors = (paletteKey) => {
   return props.traces.map((trace, index) => {
     const colorsArray = colorPalettes[paletteKey]
@@ -291,14 +272,24 @@ const relayout = () => {
   Plotly.relayout(myId.value, {})
 }
 const init = () => {
+  const modebarButtons = []
+  if (!props.disableCVD) {
+    modebarButtons.push({
+      name: 'cvd-dropdown',
+      title: 'CVD Palette',
+      icon: {
+        svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M512 256c0 .9 0 1.8 0 2.7c-.4 36.5-33.6 61.3-70.1 61.3L344 320c-26.5 0-48 21.5-48 48c0 3.4 .4 6.7 1 9.9c2.1 10.2 6.5 20 10.8 29.9c6.1 13.8 12.1 27.5 12.1 42c0 31.8-21.6 60.7-53.4 62c-3.5 .1-7 .2-10.6 .2C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm0-96a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM288 96a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm96 96a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>'
+      },
+      click: () => {
+        dropdownCVD.value = !dropdownCVD.value
+      }
+    })
+  }
   const graphDiv = myId.value
   Plotly.newPlot(graphDiv, props.traces, layoutLocal.value, {
     responsive: true,
-    displayModeBar: 'hover'
-  }).then(() => {
-    if(!props.disableCVD){
-      addCustomModebarButton()
-    }
+    displayModeBar: 'hover',
+    modeBarButtonsToAdd: modebarButtons
   })
   if (document.documentElement.clientWidth < 576) {
     Plotly.relayout(graphDiv, { showlegend: false })
