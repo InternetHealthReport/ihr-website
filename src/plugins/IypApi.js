2a20f0249250b97324822c4665851a45e726e8f4
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-05-20 18:44:56 +0900

    add code for neo4j query api (iyp); not activated yet (in comments)

--- src/plugins/IypApi.js
+++ src/plugins/IypApi.js
@@ -1,3 +1,80 @@
+// // connector for new neo4j api
+// import axios from 'axios'
+// import cache from './cache.js'
+// import { get } from 'idb-keyval'
+
+// /// Base url for api
+// const IYP_API_BASE = 'https://iyp.iijlab.net/iyp/db/neo4j/query/v2'
+// /// Default timeout before api call are considered failed
+// const DEFAULT_TIMEOUT = 180000
+
+// const IypApi = {
+//   install: (app, options) => {
+//     const axios_base = axios.create({
+//       baseURL: IYP_API_BASE,
+//       timeout: DEFAULT_TIMEOUT
+//     })
+
+//     const run = async (queries) => {
+//       const storageAllowed = JSON.parse(await get('storage-allowed'))
+//       let response = await Promise.all(queries.map(query => {
+//         return cache(
+//           JSON.stringify(query),
+//           () => {
+//             return axios_base.post('', query)
+//           },
+//           {
+//             storageAllowed: storageAllowed ? storageAllowed : false
+//           }
+//         )
+//       }))
+//       return response.map(res => {
+//         return formatResponse(res.data.data)
+//       })
+//     }
+
+//     const formatResponse = (results) => {
+//       const list = []
+//       const keys = results.fields
+//       let countElementsInRow = 0
+//       let obj = {}
+//       results.values.forEach(res => {
+//         if (countElementsInRow === 0) {
+//           obj = {}
+//         }
+//         if (typeof res === 'object') {
+//           if (res?.properties) {
+//             obj[keys[countElementsInRow]] = res.properties
+//           } else {
+//             obj[keys[countElementsInRow]] = res?.map(val => {
+//               if (val?.properties !== undefined) {
+//                 return val.properties
+//               }
+//               return val
+//             })
+//           }
+//         } else {
+//           obj[keys[countElementsInRow]] = res
+//         }
+//         countElementsInRow += 1
+//         if (countElementsInRow === keys.length) {
+//           countElementsInRow = 0
+//           list.push(obj)
+//         }
+//       })
+//       return list
+//     }
+
+//     const iyp_api = {
+//       run
+//     }
+//     app.provide('iyp_api', iyp_api)
+//   }
+// }
+
+// export { IypApi }
+
+// connector for old neo4j api
 import axios from 'axios'
 import cache from './cache.js'
 import { get } from 'idb-keyval'
