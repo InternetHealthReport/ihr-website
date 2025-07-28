85c7b7b93bc1b5ae43b51f96d97c793639ed3dba
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-07-28 18:07:55 +0900

    add new API link for swagger

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -6,6 +6,7 @@ import 'swagger-ui/dist/swagger-ui.css'
 onMounted(() => {
   SwaggerUI({
     url: 'https://ihr.iijlab.net/ihr/api/swagger.yaml?1234',
+    // url: 'https://www.ihr.live/api-dev/openapi.json', // New API
     dom_id: '#swagger-ui',
     layout: 'BaseLayout',
     defaultModelsExpandDepth: -1,
@@ -18,7 +19,7 @@ onMounted(() => {
 <template>
   <div class="row justify-center">
     <div class="col-10">
-      <div id="swagger-ui" />
+      <div id="swagger-ui"></div>
     </div>
   </div>
 </template>
